e76652c616d30a80fa1b757631ddeb4218f2ed4b
Nelson Fernandez <nelsonf94@gmail.com>
2025-11-10 20:47:11 -0600

    Add ecommerce jurisdiction entity and validation functions
    - Introduced `IEcommerceJurisdiction` interface to define the structure of ecommerce jurisdiction entities, including properties like taxId, jurisdictionId, amount, and customerCountry.
    - Implemented validation functions to ensure required fields and positive amounts are correctly validated during the creation of jurisdiction instances.

---
+++ service/src/app/domain/entities/ecommerce-jurisdiction.entity.ts
@@ -0,0 +1,42 @@
+import DomainValidatorUtils from "@domain/utils/validator-domain.util";
+
+/**
+ * Ecommerce Jurisdiction entity
+ */
+export interface IEcommerceJurisdiction {
+	readonly taxId: string;
+	readonly jurisdictionId: string;
+	readonly amount: number;
+	readonly customerCountry: string;
+}
+
+// ============================================
+// Commands (Escritura - Retornan nueva instancia)
+// ============================================
+
+function validate(jurisdiction: IEcommerceJurisdiction): void {
+	DomainValidatorUtils.validateRequiredString('taxId', jurisdiction.taxId);
+	DomainValidatorUtils.validateRequiredString('jurisdictionId', jurisdiction.jurisdictionId);
+	DomainValidatorUtils.validatePositiveNumber('amount', jurisdiction.amount);
+	DomainValidatorUtils.validateRequiredString('customerCountry', jurisdiction.customerCountry);
+}
+
+function create(
+	taxId: string,
+	jurisdictionId: string,
+	amount: number,
+	customerCountry: string
+): IEcommerceJurisdiction {
+	const item = { taxId, jurisdictionId, amount, customerCountry };
+	validate(item);
+
+	return item;
+}
+
+
+export const EcommerceJurisdiction = {
+	create,
+	validate,
+} as const;
+
+export default EcommerceJurisdiction;
